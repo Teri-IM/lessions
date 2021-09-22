4c8c563f8133a5ef47b8fb729fde75bdcec94135
Teri-IM <63192118+Teri-IM@users.noreply.github.com>
2021-09-22 21:52:34 +0300

    Initial commit

---
+++ src/module1/Less1.java
@@ -0,0 +1,12 @@
+package module1;
+
+import java.util.Scanner;
+
+public class Less1 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int y = in.nextInt();
+        System.out.println(x+y);
+    }
+}
