65631a3ca43a05724cad6ee0aa1b7ef995855537
Teri <teri.dev22@gmail.com>
2021-09-23 21:37:46 +0300

    Less2_1 upload

---
+++ src/module1/Less2_1.java
@@ -0,0 +1,11 @@
+package module1;
+
+import java.util.Scanner;
+public class Less2_1 {
+    public static void main(String[] Args){
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int c=x%10;
+        System.out.println(c);
+    }
+}
