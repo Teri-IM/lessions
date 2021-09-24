48aeceb4b7526aa1f0b2f9c716692d88918cdd2c
Teri <teri.dev22@gmail.com>
2021-09-24 15:41:15 +0300

    Less2_1_3 upload

--- src/module1/less2_1_3.java
+++ src/module1/less2_1_3.java
@@ -5,6 +5,7 @@ public class less2_1_3 {
     public static void main(String[] Args){
         Scanner in =new Scanner(System.in);
         int a=in.nextInt();
-        if(a%2==0){int b=a+2;} else {int b=a+1;}
+        int b=(a+2)-a%2;
+        System.out.println(b);
     }
 }
