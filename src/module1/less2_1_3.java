7f2f534d3d4ab48c0a830cb41a9f73885847959c
Teri <teri.dev22@gmail.com>
2021-09-24 15:26:01 +0300

    Less2_1_3 upload

---
+++ src/module1/less2_1_3.java
@@ -0,0 +1,10 @@
+package module1;
+
+import java.util.Scanner;
+public class less2_1_3 {
+    public static void main(String[] Args){
+        Scanner in =new Scanner(System.in);
+        int a=in.nextInt();
+        if(a%2==0){int b=a+2;} else {int b=a+1;}
+    }
+}
